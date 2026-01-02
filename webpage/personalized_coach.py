b3f38f2a55b42db06ca601c4ee5e4b975bcbc02e
Rohit Sharma <iro1hit@gmail.com>
2026-01-02 12:44:24 +0530

    Completed chat app UI dev and interaction. Next will be to include media files, different chat windows and database connection.

--- webpage/personalized_coach.py
+++ webpage/personalized_coach.py
@@ -1,30 +1,36 @@
 import streamlit as st
-from typing import Str, Dict
+from typing import Dict
 # Once langchain and fastapi are implemented, remove this
-import random
+import numpy as np
+import time
 
 if "messages" not in st.session_state:
-    st.session_state.messagess = Dict()
+    st.session_state.messages = []
 
-def call_chat_model(msg: Str) -> Str:
+def call_chat_model(msg: str) -> Str:
     res = [
         "This is answer to q1",
         "This is answer to q2",
         "This is answer to q3",
         "This is answer to q4"
     ]
-    res_index = random.random.int()
+    res_index = np.random.randint(0,len(res))
     return res[res_index]
 
+st.title("Your Personalized Coach")
 with st.container(border=True):
 
     with st.chat_message('assistant'):
         st.write("Hello, How can I help you?")
-    
-    with st.chat_message('user'):
-        st.write("Here is question 1?")
 
-    user_msg = st.chat_input("What is up?")
-    st.session_state.messages.add('user', user_msg)
-    assistant_msg = call_chat_model(user_msg)
-    st.session_state.messages.add('assistant', assistant_msg)
+    for msg in st.session_state.messages:
+        with st.chat_message(msg['role']):
+            st.write(msg['content'])
+            # time.sleep(5)
+
+    if user_msg := st.chat_input("What is up?"):
+        asst_msg = call_chat_model(user_msg)
+        st.session_state.messages.append({'role': 'user', 'content': user_msg})
+        st.session_state.messages.append({'role': 'assistant', 'content': asst_msg})
+        # time.sleep(5)
+        st.rerun()
