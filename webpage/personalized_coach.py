884b18e075700385e93c9a14afd79a7903a07588
Rohit Sharma <iro1hit@gmail.com>
2026-01-02 19:57:12 +0530

    1. Creating media folders for chat app, 2. Added feature where chat app can handle multiple attachments (images and videos).

--- webpage/personalized_coach.py
+++ webpage/personalized_coach.py
@@ -3,11 +3,25 @@ from typing import Dict
 # Once langchain and fastapi are implemented, remove this
 import numpy as np
 import time
+import os
+import datetime
+
+chat_media_path = os.path.join(os.getcwd(), "webpage", "utility", "chat_media")
+if os.path.exists(chat_media_path) == False:
+    os.mkdir(chat_media_path)
+
+img_media_path = os.path.join(os.getcwd(), "webpage", "utility", "chat_media", "images")
+if os.path.exists(img_media_path) == False:
+    os.mkdir(img_media_path)
+
+vid_media_path = os.path.join(os.getcwd(), "webpage", "utility", "chat_media", "videos")
+if os.path.exists(vid_media_path) == False:
+    os.mkdir(vid_media_path)
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-def call_chat_model(msg: str) -> Str:
+def call_chat_model(msg: str) -> str:
     res = [
         "This is answer to q1",
         "This is answer to q2",
@@ -30,19 +44,47 @@ with st.container(border=True):
 
     total_msgs = len(st.session_state.messages)
     i=0
-    for msg in st.session_state.messages:
+    for i, msg in enumerate(st.session_state.messages):
         if i==total_msgs-1 and msg['role']=='assistant':
             with st.chat_message(msg['role']):
                 st.write_stream(get_stream_obj(msg['content']), cursor='||')
         else:
             with st.chat_message(msg['role']):
                 st.write(msg['content'])
-            i+=1
+                if msg['role']!='assistant' and len(msg['media_file_path']) != 0:
+                    for media_file_path in msg['media_file_path']:
+                        if media_file_path.lower().endswith('.mp4'):
+                            st.video(media_file_path, width=200)
+                        elif media_file_path.lower().endswith(('.jpg', '.jpeg', 'png')):
+                            st.image(media_file_path, width=200)
+        i+=1
                 # time.sleep(5)
 
-    if user_msg := st.chat_input("What is up?"):
-        asst_msg = call_chat_model(user_msg)
-        st.session_state.messages.append({'role': 'user', 'content': user_msg})
-        st.session_state.messages.append({'role': 'assistant', 'content': asst_msg})
+    if user_msg := st.chat_input("What is up?", accept_file='multiple', file_type=['jpg', 'png', 'jpeg', 'mp4'], ):
+        asst_msg = call_chat_model(user_msg.text)
+
+        media_file_path = [] 
+        for file in user_msg["files"]:
+            username = np.random.randint(100000, 999999)
+            
+            if file.name.lower().endswith(('.mp4')):
+                vid_save_filepath = os.path.join(vid_media_path, str(username) + '_' + str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')) + '.mp4')
+                with open(vid_save_filepath, 'wb') as f:
+                    file.seek(0)
+                    f.write(file.read())
+                f.close()
+                media_file_path.append(vid_save_filepath)
+            
+            elif  file.name.lower().endswith(('jpg', 'jpeg', 'png')):
+                img_save_filepath = os.path.join(img_media_path, str(username) + '_' + str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')) + '.jpg')
+                with open(img_save_filepath, 'wb') as f:
+                    file.seek(0)
+                    f.write(file.read())
+                f.close()
+                media_file_path.append(img_save_filepath)
+        
+        st.session_state.messages.append({'role': 'user', 'content': user_msg.text, 'media_file_path': media_file_path})
+        st.session_state.messages.append({'role': 'assistant', 'content': asst_msg, 'media_file_path': None})
+
         # time.sleep(5)
         st.rerun()
